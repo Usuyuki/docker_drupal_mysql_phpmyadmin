5e2031eb6ee2042d050d53e0ac3397c6961ae0cd
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-05-13 23:05:14 +0900

    wip これだと動く

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Drupal と PHPMyAdmin が動く docker 雛形です。
 ## ローカルホスト
 
 ローカルホスト WordPress
-http://localhost:1010
+http://localhost:2020
 
 ローカルホスト PHPMyAdmin
-http://localhost:1111
+http://localhost:2222
 
 ## コマンド
 
@@ -43,3 +43,5 @@ docker-compose down
 かなり加筆修正して利用してます。
 
 https://pu10g.com/node/38
+https://qiita.com/nasuB7373/items/523f1392d87dffb5521d
+https://qiita.com/ShinKano/items/9a885473adda333ec375
--- del/docker-entrypoint.sh
+++ del/docker-entrypoint.sh
@@ -7,9 +7,12 @@ if [ ! -d "/tmp/check" ]; then
   mkdir -p /opt/drupal/web/sites/default/files/translations/
   curl https://ftp.drupal.org/files/translations/all/drupal/drupal-${DRUPAL_VERSION}.ja.po \
     -o /opt/drupal/web/sites/default/files/translations/drupal-${DRUPAL_VERSION}.ja.po
+
+  curl https://ftp.drupal.org/files/translations/all/drupal/default.settings.php \
+    -o /opt/drupal/web/sites/default/default.settings.php
   sleep 30s && drush si -y ${PROFILE} \
     --account-name="admin" --account-pass="admin" --account-mail="test@example.com" \
-    --db-url="mysql://root:password@mysql:3306/drupal" \
+    --db-url="db://user:user_pass_tekitou@mysql:3306/drupal_db" \
     --site-mail="test@example.com" --site-name="Drupal Test" \
     --locale="ja"
   drush config-set system.site uuid e707291e-7532-46d1-822c-0983cb676f8d
